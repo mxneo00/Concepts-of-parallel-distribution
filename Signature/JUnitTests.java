cc59d9909b397889c083ed23f6da306c33e5742f
Katellyn Hyker <llyn00@yahoo.com>
2025-12-16 23:41:18 -0500

    Tests explaination continuation

--- Signature/JUnitTests.java
+++ Signature/JUnitTests.java
@@ -3,8 +3,11 @@ import org.junit.jupiter.api.Test;
 
 public class JUnitTests {
     // Unit Test: verifies individual components of the system
+        // Testing specific methods or classes in isolation
     // Integration Test: checks the interaction between different components
+        // Testing combined functionality of methods and classes between two simulated players
     // System Test: validates the complete and integrated software product
+        // Testing the entire system's functionality with all simulated players and ensure player E performs as expected
 
     // Test for Minmum Word Count (Testing only the minimum word count requirement)
     @Test
